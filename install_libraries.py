786d2fd03af18a8950016f59c6545b7a718ab3cd
varadabhyankar <46650671+varadabhyankar@users.noreply.github.com>
2023-12-02 15:27:35 -0600

    Add files via upload

---
+++ install_libraries.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+
+# Install packages
+subprocess.run([sys.executable, "-m", "pip", "install", "numpy"])
+subprocess.run([sys.executable, "-m", "pip", "install", "sklearn"])
+subprocess.run([sys.executable, "-m", "pip", "install", "flask"])
+subprocess.run([sys.executable, "-m", "pip", "install", "scipy"])
+subprocess.run([sys.executable, "-m", "pip", "install", "nltk"])
+subprocess.run([sys.executable, "-m", "pip", "install", "gensim"])
+subprocess.run([sys.executable, "-m", "pip", "install", "pypdf"])
+subprocess.run([sys.executable, "-m", "pip", "install", "glob2"])
+subprocess.run([sys.executable, "-m", "pip", "install", "flask", "pymysql"])
+
+# Download NLTK data
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
